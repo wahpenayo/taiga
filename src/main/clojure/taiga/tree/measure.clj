a71bd4eb90a422b80ebc831dd0be81b1abea9997
John McDonald <palisades.lakes@gmail.com>
2017-10-30 22:46:14 -0700

    regression tree to empirical probability tree

---
+++ src/main/clojure/taiga/tree/measure.clj
@@ -0,0 +1,65 @@
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed) 
+(ns ^{:author "palisades dot lakes at gmail dot com" 
+      :since "2017-10-30"
+      :date "2017-10-30"
+      :doc "probability measure prediction." }
+    
+    taiga.tree.measure
+  
+  (:require [zana.api :as z]
+            [taiga.tree.node :as node])
+  
+  (:import [java.util HashMap Map]
+           [clojure.lang IFn]
+           [zana.prob.measure RealProbabilityMeasure]
+           [taiga.tree.node Node]
+           [taiga.tree.leaf.double Leaf]))
+;;----------------------------------------------------------------
+(defn- prediction-function [^Node root 
+                            ^Map measures]
+  "assumes <code>measures</code> is a map from <code>Leaf</code>
+   to <code>RealProbabilityMeasure</code> and has a value
+   for every leaf under <code>root</code>."
+  (fn ^zana.prob.measure.RealProbabilityMeasure [^Map predictors
+                                                 ^Object datum]
+    (let [^Leaf leaf (node/leaf root predictors datum)
+          ^RealProbabilityMeasure rpm (.get measures leaf)]
+      (assert (not (nil? rpm)))
+      rpm)))
+;;----------------------------------------------------------------
+;; TODO: should this return an instance of some prediciton model
+;; class, rather than just a simple function? 
+;; So that it can be examined?
+(defn train
+  
+  "Wrap a decision tree with a prediction function, whose values 
+   will be probability measures (instances of 
+   <code>zana.prob.measure.RealProbabilityMeasure</code>).
+   Arguments to the prediction function will be:
+   <dl>
+   <dt><code>predictors</code></dt>
+   <dd>a <code>java.util.Map</code> from keywords to functions that can be
+   applied to the elements of <code>data</code>. Must have key-value 
+   (keyword, function) pairs for the split attributes encountered between 
+   <code>node</code> and the resulting leaf.</dd>
+   <dt><code>datum</code></dt>
+   <dd>an object to which the predictor functions can be applied.</dd>
+   </dl>.
+   The returned probability measure will be estimated from the
+   <code>training-data</code> that ends up in the same leaf as the
+  later <code>datum</code> argument."
+  
+  ^clojure.lang.IFn [^Node root 
+                     ^Map predictors 
+                     ^Iterable training-data]
+  (let [leaf-ground-truth (node/leaf-ground-truth 
+                            root predictors training-data)
+        leaf-to-measure (HashMap.)]
+  (z/mapc (fn collect-measures [^Node l]
+              (let [^doubles ldata (.get leaf-ground-truth l)]
+                (.put leaf-to-measure l (z/make-wepdf ldata))))
+            (.keySet leaf-ground-truth))
+    leaf-to-measure))
+
+;;----------------------------------------------------------------
--- src/main/clojure/taiga/tree/node.clj
+++ src/main/clojure/taiga/tree/node.clj
@@ -1,12 +1,22 @@
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed) 
-(ns ^{:author "John Alan McDonald, Kristina Lisa Klinkner" :date "2016-11-11"
+(ns ^{:author ["John Alan McDonald" 
+               "Kristina Lisa Klinkner"
+               "palisades dot lakes at gmail dot com"]
+      :since "2016-11-11"
+      :date "2017-10-30"
       :doc "deftype/clojure.zipper representation of decision trees." }
     
-    taiga.tree.node)
-;;------------------------------------------------------------------------------
+    taiga.tree.node
+  
+  (:require [zana.api :as z])
+  
+  (:import [java.util ArrayList HashMap List Map]
+           [clojure.lang IFn$OD]
+           [com.carrotsearch.hppc DoubleArrayList]))
+;;----------------------------------------------------------------
 ;; decision tree representation with clojure.zipper support
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 ;; Nodes are also functions. Invoking a node on a datum descends to a leaf
 ;; and invokes that leaf on the datum.
 (definterface Node
@@ -25,25 +35,25 @@
                                        ^taiga.tree.node.Node child1])
   (^boolean isFertile [])
   (^Iterable getChildren []))
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 ;; Functional API
 ;; TODO: do we need these?
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 (defn leaf? [^Node node] (.isLeaf node))
 (defn true-child ^taiga.tree.node.Node [^Node node] (.trueChild node))
 (defn false-child ^taiga.tree.node.Node [^Node node] (.falseChild node))
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 (defn with-children ^taiga.tree.node.Node [^Node node children]
   (.withChildren node children))
 (defn fertile? [^Node node] (.isFertile node))
 (defn children ^Iterable [^Node node] (.getChildren node))
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 (defn child ^taiga.tree.node.Node 
             [^Node node ^java.util.Map predictors datum path]
   (.child node predictors datum))
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 ;; interface dependent functions
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 (defn leaf 
   
   "A method in the taiga.tree.Node protocol.<br>
@@ -63,9 +73,9 @@
   
   ^clojure.lang.IFn$OOD [^Node node ^java.util.Map predictors datum]
   (let [^Node c (.child node predictors datum)]
-  (if (.isLeaf c)
-    c
-    (recur c predictors datum))))
+    (if (.isLeaf c)
+      c
+      (recur c predictors datum))))
 
 (defn branch ^Iterable [^Node node ^java.util.Map predictors datum path]
   (let [path (conj path node)
@@ -73,9 +83,57 @@
     (if (.isLeaf c)
       path
       (recur c predictors datum path))))
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
+;; TODO: function that returns all the leaves?
+;;----------------------------------------------------------------
 ;; tree statistics
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
+;;
+(defn leaf-data
+  
+  "Return a map from leaf node to List, where
+   the list contains the data that ends up in that leaf." 
+  
+  ^java.util.Map [^Node root 
+                  ^Map predictors 
+                  ^Iterable data]
+  (let [^Map leaf-to-data (HashMap. (z/count data))]
+    (z/mapc (fn update-map [datum]
+              (let [^Node l (leaf root predictors datum)
+                    _(assert (not (nil? l)))
+                    ^List ldata (.getOrDefault leaf-to-data l (ArrayList.))]
+                (.add ldata datum)
+                (.put leaf-to-data l ldata)))
+            data)
+    leaf-to-data))
+;;----------------------------------------------------------------
+;; TODO: what about leaves that get no data?
+(defn leaf-ground-truth
+  
+  "Return a map from leaf node to double[], where
+   the list contains the ground truth values for the data that
+   ends up in that leaf.
+   Only for double-valued ground truth for now." 
+  
+  ^java.util.Map [^Node root 
+                  ^Map predictors 
+                  ^Iterable data]
+  (let [^Map leaf-to-data (HashMap. (z/count data))
+        ^IFn$OD y (:ground-truth predictors)]
+    (assert (not (nil? y)))
+    (z/mapc (fn update-map [datum]
+              (let [^Node l (leaf root predictors datum)
+                    _(assert (not (nil? l)))
+                    ^DoubleArrayList ldata (.getOrDefault leaf-to-data l (DoubleArrayList.))]
+                (.add ldata (.invokePrim y datum))
+                (.put leaf-to-data l ldata)))
+            data)
+    (z/mapc (fn list-to-array [^Node l]
+              (let [^DoubleArrayList ldata (.get leaf-to-data l)]
+                (.put leaf-to-data l (.toArray ldata))))
+            (.keySet leaf-to-data))
+    leaf-to-data))
+;;----------------------------------------------------------------
 (defn count-leaves 
   
   "How many leaves in the subtree rooted at <code>node</code>?
@@ -90,7 +148,7 @@
     (+ (count-leaves (.trueChild node))
        (count-leaves (.falseChild node)))
     1))
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 (defn count-children 
   
   "How many nodes in the subtree rooted at <code>node</code>?
@@ -106,9 +164,9 @@
        (count-children (.trueChild node))
        (count-children (.falseChild node)))
     1))
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
 (defn node-height 
- 
+  
   "What is the maximum number of nodes in a path from this <code>node</code>
    to a leaf, counting <code>node</code> and the leaf?
    <dl>
@@ -122,4 +180,4 @@
     (+ 1 (max (node-height (.trueChild node))
               (node-height (.falseChild node))))
     1))
-;;------------------------------------------------------------------------------
+;;----------------------------------------------------------------
